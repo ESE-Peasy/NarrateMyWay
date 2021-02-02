afe38892e8dec2f62170148c7bf846893c1b4a98
Ashwin Maliampurakal <45148261+Ashwin-MJ@users.noreply.github.com>
2021-02-02 10:26:48 +0000

    Initialise React Native project (#3)
    * Initialise template project
    
    * Some colour modifications
    
    * Add .expo/ to .gitignore

---
+++ app/constants/DefaultColors.ts
@@ -0,0 +1,39 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+const darkBlue = '#043964';
+
+export default {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: darkBlue,
+    tabIconDefault: '#ccc',
+    tabIconSelected: darkBlue,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
+  backgroundDarkBlue: darkBlue,
+  navBar: {
+    text: '#fff',
+    background: darkBlue,
+    tint: '#fff',
+    tabIconDefault: darkBlue,
+    tabIconSelected: '#fff',
+  },
+  customTheme: {
+    dark: false,
+    colors: {
+      primary: '#fff',
+      background: darkBlue,
+      card: darkBlue,
+      text: '#fff',
+      border: 'rgb(199, 199, 204)',
+      notification: 'rgb(255, 69, 58)',
+    },
+  },
+};
