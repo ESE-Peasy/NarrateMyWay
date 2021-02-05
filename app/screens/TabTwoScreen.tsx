b0c2f549bae0b6ecb40dbdb105452819e4c61a6d
mhr <2247469r@student.gla.ac.uk>
2021-02-05 11:28:43 +0000

    Add accessibility label for iOS compatability

--- app/screens/TabTwoScreen.tsx
+++ app/screens/TabTwoScreen.tsx
@@ -16,6 +16,7 @@ export default function TabTwoScreen() {
         darkColor="rgba(255,255,255,0.1)"
       />
       <Image
+        accessible={true}
         source={require('../assets/images/icon.png')}
         style={{ width: 100, height: 100 }}
         accessibilityLabel="Accessible image"
