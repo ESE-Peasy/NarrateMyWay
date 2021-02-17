d2951cd493cd4ef0d400dc50498e566ed47a93f6
Ashwin-MJ <ashwinmj@gmail.com>
2021-02-17 10:21:31 +0000

    Remove unecessary speech function

--- app/screens/ScanningScreen.tsx
+++ app/screens/ScanningScreen.tsx
@@ -4,8 +4,6 @@ import { StyleSheet } from 'react-native';
 import { View } from '../components/Themed';
 import ScanningButton from '../components/ScanningButton';
 
-import * as Speech from 'expo-speech';
-
 export default function ScanningScreen() {
   return (
     <View style={styles.container}>
@@ -14,26 +12,10 @@ export default function ScanningScreen() {
   );
 }
 
-const speak = () => {
-  const date = new Date();
-  const time = date.getHours() + ' ' + date.getMinutes();
-  const words = 'I am now saying some words at ';
-  Speech.speak(words + time);
-};
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold'
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%'
   }
 });
