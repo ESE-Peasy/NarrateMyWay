9aba2431750ed6775e6e4c0766f2cf26fae966f5
C-Begley <35376967+C-Begley@users.noreply.github.com>
2021-03-04 12:06:47 +0000

    23 refactor ble code (#34)
    * Untested BLE code
    
    * Added Timeout
    
    * Handle Error
    
    * Added interval
    
    * Add interval 2
    
    * Scanning with Timeouts
    
    * Updated scanning
    
    * Beacon out of scope
    
    * Working blueooth scanning and refreshing
    
    * Updated Docs
    
    * Moved ble.ts from BLE folder to src folder
    
    * Fixed spelling mistake

---
+++ app/src/ble.ts
@@ -0,0 +1,56 @@
+import { BleManager, Device } from 'react-native-ble-plx';
+import {
+  beaconDetected,
+  beaconOutOfRange
+} from '../src/state/bluetooth/actions';
+
+import { useDispatch } from 'react-redux';
+
+const THRESHOLD = -70; // in dB
+const TIMEOUT = 2000; // in ms
+
+const scanForBeacons = (manager: BleManager) => {
+  const dispatch = useDispatch();
+  let devices: Device[] = [];
+
+  setInterval(() => {
+    devices = [];
+    setTimeout(() => {
+      if (devices.length > 0) {
+        let closest = devices[0];
+        for (const dev of devices) {
+          if (dev.rssi && closest.rssi) {
+            if (dev.rssi > closest.rssi) {
+              closest = dev;
+            }
+          }
+          if (closest.rssi && closest.name) {
+            if (closest.rssi > THRESHOLD) {
+              dispatch(beaconDetected(closest.name, closest.id));
+            } else {
+              dispatch(beaconOutOfRange());
+            }
+          }
+        }
+      } else {
+        dispatch(beaconOutOfRange());
+      }
+    }, TIMEOUT);
+  }, 2 * TIMEOUT);
+
+  manager.startDeviceScan(null, null, (error, device) => {
+    if (error) {
+      manager.stopDeviceScan();
+      console.log(error.reason);
+    }
+
+    if (device && device.name && device.rssi) {
+      if (device.name.startsWith('nmw')) {
+        if (device.rssi > THRESHOLD) {
+          devices.push(device);
+        }
+      }
+    }
+  });
+};
+export default scanForBeacons;
--- app/src/state/bluetooth/reducer.tsx
+++ app/src/state/bluetooth/reducer.tsx
@@ -6,19 +6,30 @@ import {
 import { BEACON_STATE_ACTION_TYPES } from './actions';
 
 export const currentBeacon: CurrentBeacon = {};
+const DELAY_TIME = 30000; // in ms
+let lastTime = Date.now() - DELAY_TIME;
 
 function beaconStateReducer(
   state: CurrentBeacon = currentBeacon,
   action: BeaconStateAction
 ) {
+  const currentTime = Date.now();
   switch (action.type) {
     case BEACON_STATE_ACTION_TYPES.BEACON_DETECTED: {
-      const { beacon } = action as BeaconDetectedAction;
-      return beacon;
+      if (lastTime + DELAY_TIME <= currentTime) {
+        lastTime = currentTime;
+        const { beacon } = action as BeaconDetectedAction;
+        return beacon;
+      } else {
+        return state;
+      }
     }
     case BEACON_STATE_ACTION_TYPES.BEACON_OUT_OF_RANGE: {
-      console.log('The beacon is out of range');
-      return {};
+      if (lastTime + DELAY_TIME <= currentTime) {
+        return {};
+      } else {
+        return state;
+      }
     }
     default:
       return state;
