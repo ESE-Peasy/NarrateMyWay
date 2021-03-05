7eb0664a84ab1f09a4135d2d6c09b66a623190c6
Andrew Ritchie <35376993+Andrew-Ritchie@users.noreply.github.com>
2021-03-05 16:50:02 +0000

    Initial Database Development (#33)
    * Added database class
    
    * Added the nwm standard JSON file
    
    * added test to database functionality in the mainscreen.tsx file
    
    * added script to use database in the main screen file
    
    * added emblems to the database
    
    * Updated suggested changes
    
    * Updated suggested linter changes
    
    * combined SQL statements and modified callback function
    
    * fixed linter error

--- app/screens/MainScreen.tsx
+++ app/screens/MainScreen.tsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import BeaconInfo from '../components/BeaconInfo';
 import LargeButton from '../components/LargeButton';
 import { HorizontalSeparator } from '../components/Separators';
+import Storage from '../storage';
 
 import { View } from '../components/Themed';
 import { Beacon } from '../src/state/types';
@@ -13,6 +14,18 @@ import { RootStackParamList } from '../types';
 function MainScreen({
   navigation
 }: StackScreenProps<RootStackParamList, 'Main'>) {
+  // Test database functionality
+  const storage = new Storage();
+  storage.clearStorage();
+  storage.createTable();
+
+  function printLookupResult(codeDescription: String, codeEmblem: String) {
+    console.log(codeDescription);
+    console.log(codeEmblem);
+  }
+
+  storage.lookupDataForNMWCode('1-1-1', printLookupResult);
+
   return (
     <View style={styles.container}>
       <LargeButton accessibilityLabel="Tap here to repeat the previous audio output">
---
+++ app/storage.tsx
@@ -0,0 +1,115 @@
+import * as SQLite from 'expo-sqlite';
+
+import * as nmwTable from './nmwstandard.json';
+
+// Interface for data
+interface location {
+  code: string;
+  description: string;
+  emblem: String;
+}
+
+// Storage Class
+class Storage {
+  //
+  // local variables
+  db: SQLite.WebSQLDatabase;
+
+  constructor() {
+    this.db = this.createDb();
+  }
+
+  // Create database
+  createDb() {
+    const db = SQLite.openDatabase('database.db');
+    return db;
+  }
+
+  // Create and populate database tables
+  createTable() {
+    this.db.transaction((tx) => {
+      tx.executeSql(
+        'CREATE TABLE IF NOT EXISTS locationCodes (id string primary key not null, description text, emblem text);'
+      );
+      tx.executeSql(
+        'CREATE TABLE IF NOT EXISTS versionRecord (id integer primary key not null, version text);'
+      );
+      tx.executeSql('SELECT * FROM versionRecord', [], (_, results) => {
+        if (results.rows.length == 0) {
+          tx.executeSql('INSERT INTO versionRecord (version) VALUES (?)', [
+            nmwTable.version
+          ]);
+          nmwTable.nmw.forEach((value) => {
+            tx.executeSql(
+              'INSERT INTO locationCodes (id, description, emblem) VALUES (?,?,?)',
+              [value.code, value.description, value.emblem]
+            );
+          });
+        } else if (results.rows.item(0) != nmwTable.version) {
+          tx.executeSql('INSERT INTO versionRecord (version) VALUES (?)', [
+            nmwTable.version
+          ]);
+
+          nmwTable.nmw.forEach((value) => {
+            tx.executeSql(
+              'INSERT INTO locationCodes (id, description, emblem) VALUES (?,?,?)',
+              [value.code, value.description, value.emblem]
+            );
+          });
+        }
+      });
+    }, null);
+  }
+
+  // Input location code data
+  loadData(data: location) {
+    this.db.transaction((tx) => {
+      tx.executeSql(
+        'INSERT INTO locationCodes (id, description, emblem) VALUES (? ,?, ?)',
+        [data.code, data.description, data.emblem]
+      );
+    }, null);
+  }
+
+  // Clear storage
+  clearStorage() {
+    this.db.transaction((tx) => {
+      tx.executeSql('DROP TABLE locationCodes;');
+      tx.executeSql('DROP TABLE versionRecord;');
+    });
+  }
+
+  // Delete element from location table
+  deleteElementLocation(id: string) {
+    this.db.transaction((tx) => {
+      tx.executeSql('DELETE FROM locationCodes WHERE id=?', [id]);
+    });
+  }
+
+  // Print the version of currently stored data
+  printVersionData() {
+    this.db.transaction((tx) => {
+      tx.executeSql('SELECT * FROM versionRecord', [], (_, { rows }) =>
+        console.log(JSON.stringify(rows))
+      );
+    });
+  }
+
+  // Lookup code description
+  lookupDataForNMWCode(code: String, callback: Function) {
+    this.db.transaction((tx) => {
+      tx.executeSql(
+        'SELECT description, emblem FROM locationCodes WHERE id=?',
+        [code],
+        (_, results) => {
+          callback(
+            results.rows.item(0).description,
+            results.rows.item(0).emblem
+          );
+        }
+      );
+    });
+  }
+}
+
+export default Storage;
