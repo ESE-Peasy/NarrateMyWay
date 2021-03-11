3f238ae64c3d5e2db2dd98b6a2d1b233db558285
Ashwin-MJ <ashwinmj@gmail.com>
2021-03-11 17:25:02 +0000

    Eslint fix

--- app/src/state/themes/actions.tsx
+++ app/src/state/themes/actions.tsx
@@ -1,7 +1,7 @@
 import { ThemeUpdatedAction } from '../types';
 
 export enum THEME_ACTIONS {
-  THEME_UPDATED_ACTION = 'THEME/THEME_UPDATED'
+  THEME_UPDATED_ACTION = 'THEME/THEME_UPDATED' // eslint-disable-line no-unused-vars
 }
 
 export const themeUpdated = (themeName: string): ThemeUpdatedAction => ({
