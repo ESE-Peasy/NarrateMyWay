25e37d5d1c6028d9317e3084086b09ae3888fa1d
Conor Begley <conorbegley@outlook.com>
2021-03-15 18:24:19 +0000

    Intial working file download example

--- app/src/ble.ts
+++ app/src/ble.ts
@@ -3,11 +3,55 @@ import {
   beaconDetected,
   beaconOutOfRange
 } from '../src/state/bluetooth/actions';
-
 import { useDispatch } from 'react-redux';
+import * as FileSystem from 'expo-file-system';
 
 const THRESHOLD = -70; // in dB
 const TIMEOUT = 2000; // in ms
+
+const EXPANSION_PACK = 'nmw:1-exp-and';
+
+const downloadCallback = (downloadProgress: {
+  totalBytesWritten: number;
+  totalBytesExpectedToWrite: number;
+}) => {
+  console.log(
+    downloadProgress.totalBytesWritten /
+      downloadProgress.totalBytesExpectedToWrite
+  );
+};
+
+async function downloadExpansion(device: Device) {
+  console.log('Expand beacon found');
+  const location = FileSystem.documentDirectory + device.id + '.txt';
+  const present = await FileSystem.getInfoAsync(location);
+  if (present.exists == true) {
+    console.log(
+      'But file already downloaded. If you want to delete the file go to:'
+    );
+    const file = await FileSystem.getContentUriAsync(location);
+    console.log(file);
+  } else {
+    // Look up device url
+    const downloadResumable = FileSystem.createDownloadResumable(
+      'https://raw.githubusercontent.com/ESE-Peasy/NarrateMyWay/main/example_expansion_packs/expansion1.json',
+      FileSystem.documentDirectory + device.id + '.txt',
+      {},
+      downloadCallback
+    );
+    try {
+      const result = await downloadResumable.downloadAsync();
+      if (result != undefined && result.uri) {
+        console.log('Finished downloading to ', result.uri);
+        const fileContent = await FileSystem.readAsStringAsync(result.uri);
+        console.log('Finished reading file');
+        console.log(fileContent);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
 
 const scanForBeacons = (manager: BleManager) => {
   const dispatch = useDispatch();
@@ -26,7 +70,11 @@ const scanForBeacons = (manager: BleManager) => {
           }
           if (closest.rssi && closest.name) {
             if (closest.rssi > THRESHOLD) {
-              dispatch(beaconDetected(closest.name, closest.id));
+              if (closest.name == EXPANSION_PACK) {
+                downloadExpansion(closest);
+              } else {
+                dispatch(beaconDetected(closest.name, closest.id));
+              }
             } else {
               dispatch(beaconOutOfRange());
             }
@@ -38,7 +86,7 @@ const scanForBeacons = (manager: BleManager) => {
     }, TIMEOUT);
   }, 2 * TIMEOUT);
 
-  manager.startDeviceScan(null, null, (error, device) => {
+  manager.startDeviceScan(null, null, async (error, device) => {
     if (error) {
       manager.stopDeviceScan();
       console.log(error.reason);
