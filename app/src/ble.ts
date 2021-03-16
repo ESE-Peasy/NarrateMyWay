c427b5c42c27264eb5c5e616131ee03737cc00f2
Conor Begley <conorbegley@outlook.com>
2021-03-16 10:50:57 +0000

    Simple Bluetooth beacon logic for expansion packs

--- app/src/ble.ts
+++ app/src/ble.ts
@@ -11,46 +11,8 @@ const TIMEOUT = 2000; // in ms
 
 const EXPANSION_PACK = 'nmw:1-exp-and';
 
-const downloadCallback = (downloadProgress: {
-  totalBytesWritten: number;
-  totalBytesExpectedToWrite: number;
-}) => {
-  console.log(
-    downloadProgress.totalBytesWritten /
-      downloadProgress.totalBytesExpectedToWrite
-  );
-};
-
 async function downloadExpansion(device: Device) {
   console.log('Expand beacon found');
-  const location = FileSystem.documentDirectory + device.id + '.txt';
-  const present = await FileSystem.getInfoAsync(location);
-  if (present.exists == true) {
-    console.log(
-      'But file already downloaded. If you want to delete the file go to:'
-    );
-    const file = await FileSystem.getContentUriAsync(location);
-    console.log(file);
-  } else {
-    // Look up device url
-    const downloadResumable = FileSystem.createDownloadResumable(
-      'https://raw.githubusercontent.com/ESE-Peasy/NarrateMyWay/main/example_expansion_packs/expansion1.json',
-      FileSystem.documentDirectory + device.id + '.txt',
-      {},
-      downloadCallback
-    );
-    try {
-      const result = await downloadResumable.downloadAsync();
-      if (result != undefined && result.uri) {
-        console.log('Finished downloading to ', result.uri);
-        const fileContent = await FileSystem.readAsStringAsync(result.uri);
-        console.log('Finished reading file');
-        console.log(fileContent);
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  }
 }
 
 const scanForBeacons = (manager: BleManager) => {
