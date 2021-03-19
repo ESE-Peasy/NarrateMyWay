b66c676f04cf777ca2bac22fb7afa82ff3bac2a7
mhr <2247469r@student.gla.ac.uk>
2021-03-19 08:27:11 +0000

    Fix tests

--- app/__tests__/lookup.test.js
+++ app/__tests__/lookup.test.js
@@ -1,4 +1,11 @@
+import Storage from '../src/storage';
 import { normaliseNMWString } from '../src/lookup';
+
+jest.mock('../src/storage');
+
+beforeEach(() => {
+  Storage.mockClear();
+});
 
 test('Non-NMW code returns undefined', () => {
   expect(normaliseNMWString('')).toBe(undefined);
@@ -6,17 +13,21 @@ test('Non-NMW code returns undefined', () => {
 });
 
 test('Perfect NMW code not altered', () => {
-  expect(normaliseNMWString('nmw:AB-CDE-FGH')).toBe('nmw:AB-CDE-FGH');
+  expect(normaliseNMWString('nmw:AB-CDE-FGH').getPretty()).toBe(
+    'nmw:AB-CDE-FGH'
+  );
 });
 
 test('Lower case NMW code made upper case', () => {
-  expect(normaliseNMWString('nmw:ab-cde-fgh')).toBe('nmw:AB-CDE-FGH');
+  expect(normaliseNMWString('nmw:ab-cde-fgh').getPretty()).toBe(
+    'nmw:AB-CDE-FGH'
+  );
 });
 
 test('Fields after zero are zeroed out', () => {
-  expect(normaliseNMWString('nmw:AB-0-FGH')).toBe('nmw:AB-0-0');
+  expect(normaliseNMWString('nmw:AB-0-FGH').getPretty()).toBe('nmw:AB-0-0');
 });
 
 test('Field with multiple zeroes treated as zero', () => {
-  expect(normaliseNMWString('nmw:00-CDE-FGH')).toBe('nmw:0-0-0');
+  expect(normaliseNMWString('nmw:00-CDE-FGH').getPretty()).toBe('nmw:0-0-0');
 });
