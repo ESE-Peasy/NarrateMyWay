374a8195a8716c70448793adedfe6908ab368c40
Conor Begley <conorbegley@outlook.com>
2021-03-30 15:04:24 +0100

    Request service Pop up

--- app/src/ble.ts
+++ app/src/ble.ts
@@ -5,13 +5,48 @@ import {
   expansionPackDetected
 } from '../src/state/bluetooth/actions';
 import { useDispatch } from 'react-redux';
+import { Platform } from 'react-native';
+import {
+  check,
+  request,
+  PERMISSIONS,
+  RESULTS,
+  Permission
+} from 'react-native-permissions';
 
 const THRESHOLD = -100; // in dB
 const TIMEOUT = 2000; // in ms
 
 const EXPANSION_PACK = 'NMW:1-EXP-AND';
 
+const checkPermission = (permission: Permission) => {
+  check(permission).then((result) => {
+    switch (result) {
+      case RESULTS.DENIED || RESULTS.BLOCKED:
+        console.log('Permission denied');
+        console.log(permission);
+        request(permission);
+        break;
+    }
+  });
+};
+
+const checkAllPermissions = () => {
+  if (Platform.OS == 'android') {
+    console.log('android');
+    checkPermission(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+    checkPermission(PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION);
+    checkPermission(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
+    checkPermission(PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE);
+  } else if (Platform.OS == 'ios') {
+    console.log('ios');
+    checkPermission(PERMISSIONS.IOS.LOCATION_ALWAYS);
+    checkPermission(PERMISSIONS.IOS.BLUETOOTH_PERIPHERAL);
+  }
+};
+
 const scanForBeacons = (manager: BleManager) => {
+  checkAllPermissions();
   const dispatch = useDispatch();
   let devices: Device[] = [];
 
