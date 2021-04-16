fb8151a2af901ee1f0d9d7edf948f7e213ea8783
Ashwin-MJ <ashwinmj@gmail.com>
2021-04-16 10:53:01 +0100

    Linting fix

--- app/screens/ScanningScreen.tsx
+++ app/screens/ScanningScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, Alert, Platform } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 import { View } from '../components/Themed';
 import ScanningButton from '../components/ScanningButton';
